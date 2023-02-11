b840528905ab1d11305d4ae6738542898b33ea4c
muratozkan79 <muratozkan79@gmail.com>
2023-02-11 14:00:28 +0300

    some changes

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 let clock = document.getElementById("clock");
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
 
-function refreshClock() {
-  let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+if (hours < 10) {
+  hours = "0" + hours;
 }
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+if (seconds < 10) {
+  seconds = "0" + seconds;
+}
+clock.innerHTML = hours + ":" + minutes + ":" + seconds;
 
 setInterval(refreshClock, 1000);
